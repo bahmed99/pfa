63f181e7b9573378b4c51318a35546183073d87c
bahmed99 <user.email>
2021-08-18 00:01:34 +0100

    f

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -46,8 +46,8 @@ import EmploisAdmin from "./pages/a.admin/emplois/Emplois";
 import Utilisateurs from "./pages/o.admin/utilisateurs";
 import AjouterCours from "./pages/a.employe/AjouterCours";
 
-import Dashboard1 from "./pages/o.Dashboard/Dashboard1"
-import { Dashboard } from "@material-ui/icons";
+
+
 
 
 export default function MainRouter() {
@@ -142,7 +142,7 @@ export default function MainRouter() {
                     </div> : ""}
                 {(user && detect === 3) ?
                     <div>
-                        <Route exact path={"/home"} component={Dashboard1} />
+                        <Route exact path={"/home"} component={HomeAdmin} />
                         <Route exact path={"/ressources-humaine"} component={Utilisateurs} />
                         <Route exact path={"/utilisateur-profile/:id"} component={Profile1} />
                         
--- client/src/pages/a.admin/home/Home.js
+++ client/src/pages/a.admin/home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import Sidebar from '../../../components/k.employeeComponents/sidebar'
+import Dashboard from '../../../components/Dashboard/Dashboard'
+import SideBar from '../../../components/k.employeeComponents/sidebar'
 import Navbar from '../../../components/k.employeeComponents/navbar'
 
 export default function HomeAdmin() {
     return (
-        <div>
+        <div className="row">
+        <div className="col-md-2" >
             <Navbar />
-            <Sidebar />
+            <SideBar />
         </div>
+        <div className="col-md-10" style={{marginTop:"100px" , maxWidth:"1425px"}}>
+            <Dashboard />
+        </div>
+
+    </div>
     )
 }
 
